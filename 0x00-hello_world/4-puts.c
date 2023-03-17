3b8b0a06814af9cc8486b1a754a287f0236dab5d
Dimpo1 <phiridimpho@gmail.com>
2023-03-17 11:16:47 -0700

    Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #!/bin/bash
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - program that prints the given line
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
+    puts("\"Programming is like building a multilingual puzzle\n");
     return (0);
 }
