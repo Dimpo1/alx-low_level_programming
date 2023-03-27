3ed2b7e325fa25df62af6d0ff07b04403d71d115
Dimpo1 <phiridimpho@gmail.com>
2023-03-27 12:35:33 -0700

    sawp

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: Pointer to the first integer to be swapped
+ * @b: Pointer to the second integer to be swapped
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
