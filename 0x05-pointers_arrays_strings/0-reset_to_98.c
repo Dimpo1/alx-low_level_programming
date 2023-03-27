2e744e2582cf472e261f42a7e03506b32c206c39
Dimpo1 <phiridimpho@gmail.com>
2023-03-27 12:26:14 -0700

    Change

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,11 @@
-void reset_to_98(int *n) {
-    *n = 98;
+/**
+ * reset_to_98 - updates the value of the integer it points to to 98.
+ *
+ * @n: a pointer to the integer to be updated.
+ *
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+  *n = 98;
 }
